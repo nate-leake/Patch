38eccc768f8789d09a601cf2dc5d1dec8420fce9
nate-leake <nateleakenasa@gmail.com>
2023-04-01 08:57:45 -0500

    Initial Commit

---
+++ Patch/PatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PatchApp.swift
+//  Patch
+//
+//  Created by Nate Leake on 4/1/23.
+//
+
+import SwiftUI
+
+@main
+struct PatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
