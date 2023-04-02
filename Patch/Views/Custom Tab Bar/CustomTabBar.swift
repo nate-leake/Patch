bb6f5c0cddc35dd2b069ea78b7c0a0a23bafed69
nate-leake <nateleakenasa@gmail.com>
2023-04-01 20:27:05 -0500

    Added support for multiple color palettes and dark mode. Replaced Button element for TabBarButton with .onTapGesture support

--- Patch/Views/Custom Tab Bar/CustomTabBar.swift
+++ Patch/Views/Custom Tab Bar/CustomTabBar.swift
@@ -15,41 +15,48 @@ enum Tabs: Int {
 
 
 struct CustomTabBar: View {
+    @EnvironmentObject var colors:ColorContent
     
     @Binding var selectedTab: Tabs
     
     var body: some View {
         HStack{
-            Button{
-                selectedTab = .home
-            } label: {
-                TabBarButton(buttonText: "Home", imageName: "chart.line.uptrend.xyaxis.circle", isActive: selectedTab == .home)
-            }
-            .tint(colorSecondary)
+            TabBarButton(buttonText: "Home", imageName: "chart.line.uptrend.xyaxis.circle", isActive: selectedTab == .home)
+                .onTapGesture{
+                    selectedTab = .home
+                    colors.setColorPalett(name: "Original")
+                }
+                .foregroundColor(colors.Primary)
             
             
-            Button{
-                selectedTab = .categories
-            } label: {
-                TabBarButton(buttonText: "Categories", imageName: "chart.bar.doc.horizontal", isActive: selectedTab == .categories)
-            }
-            .tint(colorSecondary)
+            TabBarButton(buttonText: "Categories", imageName: "chart.bar.doc.horizontal", isActive: selectedTab == .categories)
+                .onTapGesture{
+                    selectedTab = .categories
+                    colors.setColorPalett(name: "Pleasant")
+                }
+                .foregroundColor(colors.Primary)
             
-            Button{
-                selectedTab = .accounts
-            } label: {
-                TabBarButton(buttonText: "Accounts", imageName: "creditcard", isActive: selectedTab == .accounts)
-            }
-            .tint(colorSecondary)
+            
+            
+            TabBarButton(buttonText: "Accounts", imageName: "creditcard", isActive: selectedTab == .accounts)
+                .onTapGesture {
+                    
+                    selectedTab = .accounts
+                    colors.setColorPalett(name: "System")
+                }
+            
+                .foregroundColor(colors.Primary)
             
             
         }
         .frame(height: 82)
+        
     }
 }
 
 struct CustomTabBar_Previews: PreviewProvider {
     static var previews: some View {
         CustomTabBar(selectedTab: .constant(.home))
+            .environmentObject(ColorContent())
     }
 }
