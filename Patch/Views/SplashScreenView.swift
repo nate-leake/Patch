7ce6fc0fa8a8d1b964bb302df299ba20cb98215e
nate-leake <nateleakenasa@gmail.com>
2023-04-19 14:06:30 -0500

    Added temporary splash screen image

--- Patch/Views/SplashScreenView.swift
+++ Patch/Views/SplashScreenView.swift
@@ -15,14 +15,32 @@ struct SplashScreenView: View {
     var body: some View {
         if isActive == true{
             RootView()
+                .environmentObject(colors)
+                .onAppear{
+                    colors.setPreferredColorScheme()
+                }
+            
         } else {
             ZStack{
                 colors.Fill
                     .ignoresSafeArea()
                 VStack{
-                    Image("P")
+                    Image("box")
                         .resizable()                     // Make it resizable
                         .aspectRatio(contentMode: .fit)  // Specifying the resizing mode so that image scaled correctly
+                        .overlay {
+                                LinearGradient(
+                                    colors: [colors.Primary, colors.Tertiary],
+                                    startPoint: .topLeading,
+                                    endPoint: .bottom
+                                )
+                                .mask {
+                                    Image("box")
+                                        .resizable()                     // Make it resizable
+                                        .aspectRatio(contentMode: .fit)  // Specifying the resizing mode so that image scaled correctly
+                                }
+                            }
+                    
                 }
                 .onAppear{
                     DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
