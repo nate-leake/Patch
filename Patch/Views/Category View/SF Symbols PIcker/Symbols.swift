a68d3d4f67080b7b92eb38803ac086f829b9e239
nate-leake <nateleakenasa@gmail.com>
2023-05-08 12:34:38 -0500

    Added more SFSymbols

--- Patch/Views/Category View/SF Symbols PIcker/Symbols.swift
+++ Patch/Views/Category View/SF Symbols PIcker/Symbols.swift
@@ -12,6 +12,7 @@ public enum SFCategory: String, CaseIterable, Identifiable {
     
     case transport = "Transportation"
     case tech = "Tech"
+    case personal = "Personal"
     case houseing = "Housing"
     case food = "Food"
     case finance = "Finance"
@@ -22,16 +23,24 @@ let symbols : [String: [String]] = [
     "Finance": [
         "creditcard", "giftcard", "dollarsign", "banknote"
     ],
+    
     "Food": [
         "frying.pan","popcorn","stove","cooktop","refrigerator","takeoutbag.and.cup.and.straw","wineglass","fork.knife","carrot","cup.and.saucer"
     ],
+    
     "Housing": [
-        "house", "house.lodge", "chandelier", "door.left.hand.closed"
+        "house", "house.lodge","chandelier","door.left.hand.closed"
     ],
+    
+    "Personal": [
+        "pills.fill","bathtub.fill","balloon.fill","tshirt.fill","person.fill","pencil","books.vertical.fill","comb.fill","eyeglasses","gift"
+    ],
+    
     "Tech": [
-        "airtag","display","server.rack","iphone","computermouse","dot.radiowaves.up.forward", "externaldrive"
+        "airtag","display","server.rack","iphone","computermouse","dot.radiowaves.up.forward","externaldrive","gamecontroller","cpu"
     ],
+    
     "Transportation": [
-        "figure.walk", "airplane.departure","car","bus","tram","ferry","bicycle","fuelpump"
+        "figure.walk","airplane.departure","car","bus","tram","ferry","bicycle","fuelpump","suitcase.rolling.fill","tent"
     ]
 ]
